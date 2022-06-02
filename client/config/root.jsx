7c2bac105aeb6f9d98bfd80cecc5621f56d71da0
Matthew-Av <matvey.avizhets@gmail.com>
2022-06-02 04:02:32 +0300

    adaptive styles

--- client/config/root.jsx
+++ client/config/root.jsx
@@ -6,6 +6,7 @@ import { Switch, Route, Redirect, StaticRouter } from 'react-router-dom'
 import store, { history } from '../redux'
 
 import Home from '../components/home'
+import ThePage from '../components/mpage'
 import DummyView from '../components/dummy-view'
 import NotFound from '../components/404'
 
@@ -48,7 +49,7 @@ const RootComponent = (props) => {
       <RouterSelector history={history} location={props.location} context={props.context}>
         <Startup>
           <Switch>
-            <Route exact path="/" component={DummyView} />
+            <Route exact path="/" component={ThePage} />
             <Route exact path="/dashboard" component={Home} />
             <PrivateRoute exact path="/hidden-route" component={DummyView} />
             <OnlyAnonymousRoute exact path="/anonymous-route" component={DummyView} />
